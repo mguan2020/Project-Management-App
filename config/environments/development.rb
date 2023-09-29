478031bca145840ed8fcb8086756ec74dfbc3d1b
mguan2020 <mguan988@ucla.edu>
2023-09-29 11:37:27 -0700

    Add sendgrid email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,11 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.delivery_method = :test 
+  config.action_mailer.default_url_options = { :host => 'http://ruby-on-rails-123170.nitrousapp.com:3000'}
+
+
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
